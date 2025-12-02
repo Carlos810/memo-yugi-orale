9e0ac93d378cfa4a838c020db28a17a6ac19b0a4
Juan Carlos Gomez L. <47479692+Carlos810@users.noreply.github.com>
2025-12-01 20:44:47 -0600

    Task referenciar rutas absolutas imagenes cards (#1)
    * ocultar imagen por defecto
    
    * rutas de resources into dropbox
    
    ---------
    
    Co-authored-by: carlosgomz <carlosgomz810@gmail.com,charly_magno025@hotmail.com>

--- js/LoadResources.js
+++ js/LoadResources.js
@@ -24,12 +24,13 @@ async function renderCardsToView(){
         const div = document.createElement("div");
         div.classList.add("item");
         div.setAttribute("data-id",carta["id-card"]);
-        div.setAttribute('style', 'background-image: url("assets/img/Card_Back.jpg"); background-size: cover; background-position: center; background-repeat: no-repeat;');
+        div.setAttribute('style', 'background-image: url("https://www.dropbox.com/scl/fi/e5ddp7x6t1w72y3odyoum/Card_Back.jpg?rlkey=18ig3x6gurjc7i6g3axblbomp&st=78xlfnkj&raw=1"); background-size: cover; background-position: center; background-repeat: no-repeat;');
         
         const img = document.createElement("img");
         img.src = carta["card-path"];
         img.alt = carta.name;
-        
+        // img.style.visibility = "hidden";
+
         div.appendChild(img);
         ContenedorTablero.appendChild(div);
     });
