afb4baf5ab4342a840c548d9dcc0f5da2835d687
Juan Carlos Gomez L. <47479692+Carlos810@users.noreply.github.com>
2025-12-03 01:05:06 -0600

    Voltear cartas de par en par con 1 solo jugador (#2)
    Co-authored-by: carlosgomz <carlosgomz810@gmail.com,charly_magno025@hotmail.com>

--- js/LoadResources.js
+++ js/LoadResources.js
@@ -1,9 +1,6 @@
 
 async function LoadCardsFromResources(){
-    debugger;
     let cartasDuplicadas = null
-    
-
     const loadCartas = await fetch('./assets/fakeDb/cards.json'); 
     let cartas = await loadCartas.json();
 
@@ -29,12 +26,37 @@ async function renderCardsToView(){
         const img = document.createElement("img");
         img.src = carta["card-path"];
         img.alt = carta.name;
-        // img.style.visibility = "hidden";
+        img.style.visibility = "hidden";
 
         div.appendChild(img);
         ContenedorTablero.appendChild(div);
     });
 }
 
-export {renderCardsToView};
+function RenderCardsAgain(array){
+    debugger;
+    let ContenedorTablero = document.querySelector(".Tablero");
+    ContenedorTablero.innerHTML = "";
+    array.forEach((carta) => {
+        const div = document.createElement("div");
+        div.classList.add("item");
+        div.setAttribute("data-id",carta["id-card"]);
+        div.setAttribute('style', 'background-image: url("https://www.dropbox.com/scl/fi/e5ddp7x6t1w72y3odyoum/Card_Back.jpg?rlkey=18ig3x6gurjc7i6g3axblbomp&st=78xlfnkj&raw=1"); background-size: cover; background-position: center; background-repeat: no-repeat;');
+        
+        
+        const img = document.createElement("img");
+        img.src = carta["card-path"];
+        img.alt = carta.name;
+        if(carta["revelado"] == true){
+            img.style.visibility = "visible";
+
+        }else{
+            img.style.visibility = "hidden"
+        }
+        div.appendChild(img);
+        ContenedorTablero.appendChild(div);
+    });
+}
+
+export {renderCardsToView,RenderCardsAgain};
 
